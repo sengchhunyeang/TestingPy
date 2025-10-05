0070a50ed8bda8f863e8ce56a5dd65417f497e34
kimkheang345 <kimkheangchoun@gmail.com>
2025-10-05 12:46:17 +0700

    name

--- kheang.py
+++ kheang.py
@@ -1,1 +1,15 @@
 print("haha")
+name = input(int("enter your name :")
+
+def play_ball() : 
+ print("Play ball is fun")
+    
+play_ball()
+
+#2.funtion has parameter and no return    
+def infor (first_name, last_name) :
+ 
+
+ print(f"frist name : {first_name}")
+ print(f"last name : {last_name }")
+      
